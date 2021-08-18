a4a47d7618330193118d959ddc746bb0453e87ce
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-08-17 20:48:28 -0700

    connected clearDisplay

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React, { useReducer } from 'react';
+import React, { useReducer } from "react";
 
-import './App.css';
+import "./App.css";
 
-import TotalDisplay from './components/TotalDisplay';
-import CalcButton from './components/CalcButton';
-import reducer, { initialState } from './reducers';
-import { addOne, applyNumber, moreMath } from './actions';
+import TotalDisplay from "./components/TotalDisplay";
+import CalcButton from "./components/CalcButton";
+import reducer, { initialState } from "./reducers";
+import { addOne, applyNumber, clearDisplay, moreMath } from "./actions";
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState)
-  console.log(state)
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
 
   const doMath = (num) => {
-    dispatch(applyNumber(num))
-  }
+    dispatch(applyNumber(num));
+  };
 
   const Math = (opp) => {
-    dispatch(moreMath(opp))
-  }
+    dispatch(moreMath(opp));
+  };
+
+  const reset = () => {
+    dispatch(clearDisplay());
+  };
 
   // const addNum = (num) => {
   //   dispatch(addOne(num))
@@ -26,53 +30,60 @@ function App() {
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Reducer Challenge</a>
+        <a className="navbar-brand" href="#">
+          <img width="40px" src="./Lambda-Logo-Red.png" /> Lambda Reducer
+          Challenge
+        </a>
       </nav>
 
-      <div className = "container row mt-5">
+      <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-            
-            <TotalDisplay value={state.total}/>
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b>{state.operation}</span>
-              <span id="memory"><b>Memory:</b>{state.memory}</span>
-            </div>
-            
-            <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
-            </div>
-
-            <div className="row">
-              <CalcButton onClick={() => doMath(1)} value={1}/>
-              <CalcButton onClick={() => doMath(2)} value={2}/>
-              <CalcButton onClick={() => doMath(3)} value={3}/>
+              <span id="operation">
+                <b>Operation:</b>
+                {state.operation}
+              </span>
+              <span id="memory">
+                <b>Memory:</b>
+                {state.memory}
+              </span>
             </div>
 
             <div className="row">
-              <CalcButton onClick={() => doMath(4)} value={4}/>
-              <CalcButton onClick={() => doMath(5)} value={5}/>
-              <CalcButton onClick={() => doMath(6)} value={6}/>
+              <CalcButton value={"M+"} />
+              <CalcButton value={"MR"} />
+              <CalcButton value={"MC"} />
             </div>
 
             <div className="row">
-              <CalcButton onClick={() => doMath(7)} value={7}/>
-              <CalcButton onClick={() => doMath(8)} value={8}/>
-              <CalcButton onClick={() => doMath(9)} value={9}/>
+              <CalcButton onClick={() => doMath(1)} value={1} />
+              <CalcButton onClick={() => doMath(2)} value={2} />
+              <CalcButton onClick={() => doMath(3)} value={3} />
             </div>
 
             <div className="row">
-              <CalcButton onClick={() => Math('+')} value={"+"}/>
-              <CalcButton onClick={() => Math('*')} value={"*"}/>
-              <CalcButton onClick={() => Math('-')} value={"-"}/>
+              <CalcButton onClick={() => doMath(4)} value={4} />
+              <CalcButton onClick={() => doMath(5)} value={5} />
+              <CalcButton onClick={() => doMath(6)} value={6} />
+            </div>
+
+            <div className="row">
+              <CalcButton onClick={() => doMath(7)} value={7} />
+              <CalcButton onClick={() => doMath(8)} value={8} />
+              <CalcButton onClick={() => doMath(9)} value={9} />
+            </div>
+
+            <div className="row">
+              <CalcButton onClick={() => Math("+")} value={"+"} />
+              <CalcButton onClick={() => Math("*")} value={"*"} />
+              <CalcButton onClick={() => Math("-")} value={"-"} />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton onClick={reset} value={"CE"} />
             </div>
-
           </form>
         </div>
       </div>
